41f3a1580a85e003457eab5e6cb99929de257200
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2021-08-29 00:53:57 +0300

    files changed

--- BmiService.java
+++ BmiService.java
@@ -1,25 +1,8 @@
 public class BmiService {
-    public int calculate(double weight, double height) {
+    public double calculate(double weight, double height) {
         double bmi = weight / (height * height / 100 / 100);
-        int indexBodyMass = (int) bmi;
-        if (bmi >= 16.5 && bmi <= 18.49) {
-            return indexBodyMass;
-        } if (bmi < 16.5) {
-            return indexBodyMass;
-        } if (bmi>=18.5 && bmi<=24.99) {
-           return indexBodyMass;
-        } if (bmi>=25 && bmi<=29.99) {
-            return indexBodyMass;
-        } if (bmi>=30 && bmi<=34.99) {
-            return indexBodyMass;
-        } if (bmi>=35 && bmi<=39.99) {
-            return indexBodyMass;
-        } if (bmi>=40) {
-            return indexBodyMass;
-        }
-        else
-        return indexBodyMass;
-          }
+              return (int) bmi;
+    }
 }
 
 
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int index = service.calculate(54, 174);
-                System.out.println(index);
+        double index = service.calculate(54, 174);
+               System.out.printf("%.2f",index);
     }
 }
