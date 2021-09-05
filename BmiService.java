f8301465b588c74120e161caa4c97b4ced8764f7
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2021-09-05 19:03:51 +0300

    Bmi changed

--- BmiService.java
+++ BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate(double weight, double height) {
         double bmi = weight / (height * height / 100 / 100);
-              return (int) bmi;
+              return (double) bmi;
     }
 }
 
